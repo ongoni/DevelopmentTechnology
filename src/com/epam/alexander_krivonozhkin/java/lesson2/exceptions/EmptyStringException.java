a573827eeeec4777de8e915a9366b12d2e52bcf4
ongoni <ongoni.fg@gmail.com>
2017-12-11 11:25:49 +0400

    fix

--- src/com/epam/alexander_krivonozhkin/java/lesson2/exceptions/EmptyStringException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.alexander_krivonozhkin.java.lesson2.exceptions;
-
-public class EmptyStringException extends Exception {
-
-    public EmptyStringException() {
-        super("Empty string given");
-    }
-
-}
--- src/com/epam/alexander_krivonozhkin/java/lesson2/exceptions/NegativeValueException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.alexander_krivonozhkin.java.lesson2.exceptions;
-
-public class NegativeValueException extends Exception {
-
-    public NegativeValueException() {
-        super("Negative value given");
-    }
-
-}
